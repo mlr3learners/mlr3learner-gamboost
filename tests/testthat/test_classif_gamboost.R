30e33fecf996d9319a3b89413c587218526c963c
pat-s <patrick.schratz@gmail.com>
2020-03-17 19:42:37 +0100

    style

--- tests/testthat/test_classif_gamboost.R
+++ tests/testthat/test_classif_gamboost.R
@@ -2,7 +2,7 @@ context("classif.gamboost")
 
 test_that("autotest", {
   learner = LearnerClassifGAMBoost$new()
-  learner$param_set$values = list(baselearner = "btree", family="Binomial")
+  learner$param_set$values = list(baselearner = "btree", family = "Binomial")
   expect_learner(learner)
   result = run_autotest(learner)
   expect_true(result, info = result$error)
