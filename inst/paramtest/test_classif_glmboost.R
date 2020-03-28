174c7c5715b76439b3c0e56e9e91ba7bba48be8d
be-marc <marcbecker@posteo.de>
2020-03-28 11:56:18 +0100

    Update parameter tests

--- inst/paramtest/test_classif_glmboost.R
+++ inst/paramtest/test_classif_glmboost.R
@@ -2,12 +2,13 @@ library(mlr3)
 library(mlr3learners.mboost)
 source(list.files(system.file("paramtest", package = "mlr3learners.mboost"), pattern = "^helper.*\\.[rR]", full.names = TRUE))
 
-test_that("classif.gamboost", {
-  learner = lrn("classif.gamboost")
-  fun = mboost::gamboost
+test_that("classif.glmboost", {
+  learner = lrn("classif.glmboost")
+  fun = mboost:::glmboost.formula
   exclude = c(
     "formula", # .train
     "data", # .train
+    "contrasts.arg", # ?
     "na.action", # Only na.omit and na.fail available
     "weights", # .train
     "control", # mboost::boost_control
@@ -18,8 +19,8 @@ test_that("classif.gamboost", {
   expect_true(result, info = paste0("Missing parameters:\n", paste0(result$missing, collapse = "\n")))
 })
 
-test_that("classif.gamboost_boost_control", {
-  learner = lrn("classif.gamboost")
+test_that("classif.glmboost_boost_control", {
+  learner = lrn("classif.glmboost")
   fun = mboost::boost_control
   exclude = c(
     "stopintern", # ?
