9e527f511e61d9fe448e5ff55ed5b1369f599fda
Raphael <raphael.sonabend.15@ucl.ac.uk>
2020-06-08 15:59:52 +0100

    Update helper.R

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -6,4 +6,3 @@ lapply(list.files(system.file("testthat", package = "mlr3"),
 
 lapply(list.files(system.file("testthat", package = "mlr3proba"),
                   pattern = "^helper.*\\.[rR]", full.names = TRUE), source)
-
